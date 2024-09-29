b1bb0ae041639929727d61493ee4f32865f1e90f
Sheila Diz <dizsheila07@gmail.com>
2024-09-29 19:20:02 -0300

    create spring project

---
+++ recipes-back/src/main/java/com/sheiladiz/recipes/RecipesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sheiladiz.recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesSystemApplication.class, args);
+	}
+
+}
---
+++ recipes-back/src/test/java/com/sheiladiz/recipes/RecipesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheiladiz.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
